f3ecfbc6b9dc52f75503bb1a7e897b0e27781280
jothiajith25-hue <jothiajith25@gmail.com>
2026-05-17 14:11:57 +0530

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a plant shop that offers a wide variety of
+        beautiful and healthy houseplants. Our mission is to bring nature
+        closer to your home by providing high-quality plants at affordable prices.
+      </p>
+      <p>
+        We believe plants improve air quality and create a peaceful
+        environment. Start your journey with us today!
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
